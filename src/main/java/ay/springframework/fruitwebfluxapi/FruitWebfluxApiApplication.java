dfefdf1a4eeabe4134b9988c1ce5069d1564f64a
aliyussef <a.yussef@outlook.com>
2021-03-22 20:01:25 +0100

    Initial commit

---
+++ src/main/java/ay/springframework/fruitwebfluxapi/FruitWebfluxApiApplication.java
@@ -0,0 +1,13 @@
+package ay.springframework.fruitwebfluxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitWebfluxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitWebfluxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ay/springframework/fruitwebfluxapi/FruitWebfluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ay.springframework.fruitwebfluxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitWebfluxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
